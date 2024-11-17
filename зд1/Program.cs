f8ea74b225837f4854e08f7d515d48045e2014ba
ArtyomBaryshnikov <saominamisa@gmail.com>
2024-11-17 23:26:49 +0300

    ЛР 1

---
+++ зд1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("a = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+int b = int.Parse(Console.ReadLine());
+int sum = a + b;
+Console.WriteLine($"{a}+{b}={sum}");
